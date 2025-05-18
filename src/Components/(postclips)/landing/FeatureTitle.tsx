cc39d42bdd669b6d14f73ff149f92aa4cfd2282d
pablo-postclips <pablo@postclips.com>
2025-05-17 18:30:04 -0600

    landing clipper

--- src/Components/(postclips)/landing/FeatureTitle.tsx
+++ src/Components/(postclips)/landing/FeatureTitle.tsx
@@ -5,41 +5,38 @@ import { useInView } from "framer-motion";
 import { useFeatureStore } from "./useFeatureStore";
 
 type Props = {
-  children: React.ReactNode;
-  id: string;
+    children: React.ReactNode;
+    id: string;
 };
 
 export const FeatureTitle = ({ children, id }: Props) => {
-  const ref = useRef<HTMLParagraphElement>(null);
-  const documentRef = useRef<Document | null>(null);
-  
-  // Set document ref after component mounts
-  useEffect(() => {
-    documentRef.current = document;
-  }, []);
-  
-  const isInView = useInView(ref, {
-    margin: "-60% 0px -60% 0px",
-    // We need to use the @ts-ignore because even though framer-motion
-    // works with document as root, the types don't reflect this
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    root: documentRef,
-  });
-  const setInViewFeature = useFeatureStore((state) => state.setInViewFeature);
-  const inViewFeature = useFeatureStore((state) => state.inViewFeature);
+    const ref = useRef<HTMLParagraphElement>(null);
+    const documentRef = useRef<Document | null>(null);
 
-  useEffect(() => {
-    if (isInView) setInViewFeature(id);
-    if (!isInView && inViewFeature === id) setInViewFeature(null);
-  }, [isInView, id, setInViewFeature, inViewFeature]);
+    // Set document ref after component mounts
+    useEffect(() => {
+        documentRef.current = document;
+    }, []);
 
-  return (
-    <p
-      ref={ref}
-      className={`feature-title ${isInView ? "opacity-100" : "opacity-0"}`}
-    >
-      {children}
-    </p>
-  );
+    const isInView = useInView(ref, {
+        margin: "-60% 0px -60% 0px",
+        // @ts-expect-error framer-motion works with document as root, but types don't reflect this
+        root: documentRef,
+    });
+    const setInViewFeature = useFeatureStore((state) => state.setInViewFeature);
+    const inViewFeature = useFeatureStore((state) => state.inViewFeature);
+
+    useEffect(() => {
+        if (isInView) setInViewFeature(id);
+        if (!isInView && inViewFeature === id) setInViewFeature(null);
+    }, [isInView, id, setInViewFeature, inViewFeature]);
+
+    return (
+        <p
+            ref={ref}
+            className={`feature-title ${isInView ? "opacity-100" : "opacity-0"}`}
+        >
+            {children}
+        </p>
+    );
 };
